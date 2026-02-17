0822c3fa045f08eca83ad55ab8ab04c5f615863c
Mahesh Naik <naik21.mahesh@gmail.com>
2026-02-17 10:57:31 +0530

    fix duplicate server declaration

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ const socketIo = require('socket.io');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-
 const server = app.listen(port, () => {
   console.log("Server running on port " + port);
 });
@@ -360,10 +359,7 @@ app.post('/api/putDevice', async (req, res) => {
     });
   }
 });
-
-const server = app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
+ 
 
 const io = socketIo(server, {
   cors: {
